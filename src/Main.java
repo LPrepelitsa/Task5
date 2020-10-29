6bc9539e53569aba7baa3727bab632d64f0707d7
Liudmila.Prepelita <liudmila.prepelita@endava.com>
2020-10-29 16:38:38 +0200

    Commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+
+        System.out.println(sum + " = " + a + " + " + b);
     }
 
 }
